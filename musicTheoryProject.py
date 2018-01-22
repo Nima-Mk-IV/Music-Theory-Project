de3d83f67f847e9cc6619b7edf9f363f5cc01d6f
Vikram Amin <34107035+Nima-Mk-IV@users.noreply.github.com>
2018-01-22 00:10:09 -0800

    Dictionary 1.0

--- musicTheoryProject.py
+++ musicTheoryProject.py
@@ -8,15 +8,16 @@ Oct 27, 2017
 import pandas as pd
 import re
 
-df=pd.read_csv("//Users/TheMusician/Desktop/PitchClassGroups.csv")
-
-#df['Vector']=df['Vector'].apply(lambda x: x.rstrip("0"))
+#Pitch Class Set data
+df=pd.read_csv("C:/Users/aminv/Desktop/Music-Theory-Project-master/Music-Theory-Project-master/PitchClassGroups.csv")
+df['Vector']=df['Vector'].astype(str)
+df['Vector']=df['Vector'].apply(lambda x: x.rstrip("0"))
 
 #takes in string converts to array of ints
-search_string = input('Enter search: ')
+search_string = str(input('Enter search: '))
 
 
-
+#deals with finding vector form if pitches are inputted
 def pitches_input(pitchClassesString,df):
     pitchClassesString+=','
     start=0
@@ -69,28 +70,22 @@ def pitches_input(pitchClassesString,df):
     print(input_set_df)
 
 
-if(',' in search_string):
-    pitches_input(search_string,df)
-else:
-    matched_search_df=df.loc[df['Vector'] == search_string]
-    print(matched_search_df)
-    matched_search_df=df.loc[df['Set Name'] == search_string]
-    print(matched_search_df)
-    matched_search_df=df.loc[df['Prime Form'] == search_string]
-    print(matched_search_df)
-    matched_search_df=df.loc[df['Description'] == search_string]
-    print(matched_search_df)
 
 
+#helper function for adding these types of 'vectors'
 def vector_add(vector1,vector2):
+    while(len(vector1)<len(vector2)):
+        vector1+='0'
+    while(len(vector2)<len(vector1)):
+        vector2+='0'
     summed_vector=''
     for i in range(len(vector1)):
         summed_vector+=str( int( vector1[i] ) + int( vector2[i] ) )
-    
+    summed_vector.rstrip("0")
     return summed_vector
     
-    
-def find_subsets(given_vector):
+#builds the dictionary of all two combinations
+def create_two_additions():
     two_additions_df=pd.DataFrame(columns=('Set1', 'Set2', 'Vector1','Vector2','Combined Vector'))
     for index1, row1 in df.iterrows():
         for index2, row2 in df.iterrows():
@@ -99,11 +94,50 @@ def find_subsets(given_vector):
             set2=row2['Set Name']
             vector2=row2['Vector']
             summed_vector=vector_add(vector1,vector2)
-            two_additions_df.loc[index1+index2] = [set1,set2,vector1,vector2,summed_vector]
+            two_additions_df.loc[len(two_additions_df)]=[set1,set2,vector1,vector2,summed_vector]
+    two_additions_df=two_additions_df.drop_duplicates()
+    two_additions_df['Combined Vector']=two_additions_df['Combined Vector'].apply(lambda x: x.rstrip("0"))
+    two_additions_df.to_pickle("C:/Users/aminv/Desktop/Music-Theory-Project-master/Music-Theory-Project-master/two_additions_df.pkl")
+
+#builds the dictionary of all 3 combinations
+def create_three_additions():
+    three_additions_df=pd.DataFrame(columns=('Set1', 'Set2', 'Set3', 'Vector1','Vector2','Vector3','Combined Vector'))
+    for index1, row1 in df.iterrows():
+        for index2, row2 in df.iterrows():
+            for index3, row3 in df.iterrows():
+                set1=row1['Set Name']
+                vector1=row1['Vector']
+                set2=row2['Set Name']
+                vector2=row2['Vector']
+                set3=row3['Set Name']
+                vector3=row3['Vector']
+                summed_vector=vector_add(vector1,vector2)
+                summed_vector=vector_add(summed_vector,vector3)
+                three_additions_df.loc[index1+index2] = [set1,set2,set3,vector1,vector2,vector3,summed_vector]
+    three_additions_df=three_additions_df.drop_duplicates()
+    three_additions_df['Combined Vector']=three_additions_df['Combined Vector'].apply(lambda x: x.rstrip("0"))
+    three_additions_df.to_pickle("C:/Users/aminv/Desktop/Music-Theory-Project-master/Music-Theory-Project-master/three_additions_df.pkl")
+
+#finds sets which add to a given set inputted in vector form
+def find_subsets(given_vector):
+    two_additions_df=pd.read_pickle("C:/Users/aminv/Desktop/Music-Theory-Project-master/Music-Theory-Project-master/two_additions_df.pkl")
+    three_additions_df=pd.read_pickle("C:/Users/aminv/Desktop/Music-Theory-Project-master/Music-Theory-Project-master/three_additions_df.pkl")
+
+    print('Combinations of 2 Sets:')
+    matched_search_df=two_additions_df.loc[two_additions_df['Combined Vector'] == given_vector]
+    print(matched_search_df)
     
-    matched_search_df=df.loc[df['Combined Vector'] == given_vector]
+    print('Combinations of 3 Sets:')
+    matched_search_df=three_additions_df.loc[three_additions_df['Combined Vector'] == given_vector]
     print(matched_search_df)
     
     
-    
+if(',' in search_string):
+    print('Information About Input:')
+    pitches_input(search_string,df)
+else:
+    print('Information About Input:')
+    print(matched_search_df)
+
+
     
